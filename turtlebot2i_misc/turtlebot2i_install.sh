d6eb6a7bb98091ec7e93dd951bc5bbb295bb7ea4
Solomon Wiznitzer <shlomo.wiz@gmail.com>
2021-05-27 00:58:42 -0400

    updates server address for the librealsense software in the install script

--- turtlebot2i_misc/turtlebot2i_install.sh
+++ turtlebot2i_misc/turtlebot2i_install.sh
@@ -65,7 +65,7 @@ source $ORBBEC_WS/devel/setup.bash
 if [ $(dpkg-query -W -f='${Status}' librealsense2 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
   echo "Installing librealsense2..."
   sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
-  sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main" -u
+  sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo xenial main" -u
   version="2.33.1-0~realsense0.2140"
   sudo apt -y install librealsense2-udev-rules=${version}
   sudo apt -y install librealsense2-dkms=1.3.11-0ubuntu1
